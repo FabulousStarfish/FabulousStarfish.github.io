3e223910634c82b9c26b203bac2f2bf494adaeef
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-01-20 22:31:47 +0530

    Add files via upload

--- hextris/js/main.js
+++ hextris/js/main.js
@@ -444,3 +444,32 @@ function showHelp() {
 	script.src = 'http://hextris.io/a.js';
 	document.head.appendChild(script);
 })()
+
+var endDate = localStorage.getItem("EndTime");
+
+// Update the count down every 1 second
+var x = setInterval(function() 
+{
+
+  // Get today's date and time
+  now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  distance = endDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) 
+  {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+    localStorage.clear("TicketNumber");
+    localStorage.clear("UserName");
+  }
+}, 1000);
